97fdf3916a944de34009cd7e91a3875818f1392b
MLeslie7 <mleslie7@outlook.com>
2015-07-23 15:51:04 -0500

    Create Lab-Test-Create.sql

---
+++ Lab-Test-Create.sql
@@ -0,0 +1,24 @@
+USE [LAB-Test]
+GO
+/****** Object:  Table [dbo].[Test_Table1]    Script Date: 7/23/2015 3:36:08 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Test_Table1](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Insert-Date] [smalldatetime] NOT NULL,
+	[SomeValue] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Test_Table1] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+ALTER TABLE [dbo].[Test_Table1] ADD  CONSTRAINT [DF_Test_Table1_Insert-Date]  DEFAULT (getdate()) FOR [Insert-Date]
+GO
